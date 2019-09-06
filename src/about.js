ece7c168fcaf8720dab0f6d1d64d85b9621e5180
Marcos A. Fernandes <contato@marcos-fernandes.com>
2019-09-06 10:50:53 -0300

    added breaklines in about page

--- src/about.js
+++ src/about.js
@@ -53,13 +53,13 @@ const pageAbout = () => {
       aboutText.classList.add('content');
 
       title.innerHTML = 'La Mafia di JS Ristorante';
-      aboutText.innerHTML = `Situated in a far, far away galaxy, La Mafia di JS Ristorante is a very specialized traditional italian cuisine. \n 
-                             Our history begins many years ago, when young Lucas Skywalkeri used to lunch at his nonna every sunday. \n
-                             He watched her cooking and was amused by her techniques and passion. \n
-                             Every day thoughts of becoming a great cook came to Lucas's mind. \n
-                             One day he asked his nonna to teach him how to become a master cook. \n
-                             She took his wish to the great cook concil, which wasn't found of the idea because of his temper. \n
-                             Arguing she would be held responsible for the kid, was able to get permission to at last teach the young boy. \n
+      aboutText.innerHTML = `Situated in a far, far away galaxy, La Mafia di JS Ristorante is a very specialized traditional italian cuisine. <br> 
+                             Our history begins many years ago, when young Lucas Skywalkeri used to lunch at his nonna every sunday. <br>
+                             He watched her cooking and was amused by her techniques and passion. <br>
+                             Every day thoughts of becoming a great cook came to Lucas's mind. <br>
+                             One day he asked his nonna to teach him how to become a master cook. <br>
+                             She took his wish to the great cook concil, which wasn't found of the idea because of his temper. <br>
+                             Arguing she would be held responsible for the kid, was able to get permission to at last teach the young boy. <br>
                              Lucas grew to become a great master cook and invested all his funds and efforts on what would become the greatest fictional restaurant ever.`;
 
       content.appendChild(title);
